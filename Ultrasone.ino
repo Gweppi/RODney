e5b41749805573f648ece1cfdf756b177a967376
Jesper Dinger <78754860+Gweppi@users.noreply.github.com>
2022-06-09 10:28:40 +0200

    Add files via upload

---
+++ Ultrasone.ino
@@ -0,0 +1,15 @@
+#include <NewPing.h>
+NewPing sonar (10, 11, 20);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(50);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("The distance is:");
+  Serial.println(sonar.ping_cm());
+  delay(1000);
+}
